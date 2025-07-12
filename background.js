02dc836257b617a1f62b849cfb01bf3d9647b2f0
Tin Benjamin Matuka <tin@pakd.ch>
2025-07-12 20:42:26 +0200

    Updated to manifest v3

--- background.js
+++ background.js
@@ -1,51 +1,38 @@
-var suggestions = [];
-
 chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-    var cleantext = text.replace(' ', '');
-	var url = "http://sg.media-imdb.com/suggests/" + cleantext.substr(0,1) + "/" + cleantext + ".json";
-	//console.log(url);
+  async function(text, suggest) {
+      if (text === '') {
+          suggest([]);
 
-	var request = new XMLHttpRequest();
-	request.open('GET', url, true);
-	
-	request.onreadystatechange = function()
-		{
-		if (request.readyState != 4) return;
-		
-		if (request.status === 200)
-			{
-			var offset = 6 + cleantext.length;
-			var responseText = request.responseText.replace('&', '&amp;');
-			//console.log(responseText.substr(offset, responseText.length - offset - 1));
-			var res = JSON.parse(responseText.substr(offset, responseText.length - offset - 1));
-			suggestions = [];
-			for(var i = 0; i < res.d.length; i++)
-				{
-				var content, description;
-				if('y' in res.d[i])
-					{
-					description = res.d[i].l + ' (' + res.d[i].y + ') - ' + res.d[i].s;
-					}
-				else
-					{
-					description = res.d[i].l + ' - ' + res.d[i].s;
-					}
-				
-				content = res.d[i].id;
-				
-				suggestions[i] = { "content": content, "description": description };
-				}
-			suggest(suggestions);
-			}
-		else
-			{
-			// console.log(request.status);
-			suggest(suggestions);
-			}
-		};
-	
-	request.send(null);
+          return;
+      }
+      
+    var cleantext = text.replace(' ', '');
+    var url = "http://sg.media-imdb.com/suggests/" + cleantext.substr(0,1) + "/" + cleantext + ".json";
+
+    const response = await fetch(url);
+    const responseText = await response.text();
+    
+    const offset = responseText.indexOf('({') + 1;
+    const responseJson = responseText.substring(offset, responseText.length - 1).replaceAll('&', '&amp;');
+    
+    const res = JSON.parse(responseJson);
+    
+    const suggestions = res.d.filter((result) => {
+        return !result.id.includes('/');
+    }).map((result) => {
+        const content = result.id;
+        let description = `${result.l} - ${result.s}`;
+        if (result.y) {
+            description = `${result.l} (${result.y}) - ${result.s}`;
+        }
+        
+        return {
+            content,
+            description,
+        };
+    });
+    
+    suggest(suggestions);
   });
 
 // This event is fired when the user accepts the input in the omnibox.
